114cd1652d0a28797673d344d8e0e9fd44f5f110
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2024-08-01 22:13:36 +0900

    モデルの作成

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+# クイズのジャンルモデル
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+# 問題の難易度モデル
+class Difficulty(models.Model):
+    level = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.level
+
+# 問題モデル
+class Question(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    difficulty = models.ForeignKey('Difficulty', on_delete=models.CASCADE)
+    # 問題文
+    text = models.CharField(max_length=500)
+    # 選択肢
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    # 正解
+    correct_answer = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.text
