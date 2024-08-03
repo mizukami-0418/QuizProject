639b0a66414946fc5f5bdc09e22f4dc66292edf5
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2024-08-03 14:45:35 +0900

    問題表示まで作成

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('quiz/<int:category_id>/<int:difficulty_id>/', views.quiz, name='quiz'),
+    path('question/<int:question_id>/', views.question, name='question'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Difficulty, Question
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    # ホーム画面でジャンル選択
+    category = Category.objects.all()
+    difficulty = Difficulty.objects.all()
+    return render(request, 'home.html', {'categories': category, 'difficulties': difficulty})
+
+
+def quiz(request, category_id, difficulty_id):
+    # 指定されたカテゴリと難易度に一致する問題をランダムに10問取得
+    questions = Question.objects.filter(category_id=category_id, difficulty_id=difficulty_id).order_by('?')[:10]
+    # 取得した問題のIDをリストとしてセッションに保存します。
+    request.session['questions'] = [question.id for question in questions]
+    # 現在の問題のインデックスをセッションに保存します。最初は0に設定します。
+    request.session['current_question'] = 0
+    # 現在のスコアをセッションに保存します。初期スコアは0に設定します。
+    request.session['score'] = 0
+    # 最初の問題のページにリダイレクトします。
+    return redirect('question', question_id=questions[0].id)
+
+
+def question(request, question_id):
+    # quizで取得した問題10問をquestionに格納
+    question = get_object_or_404(Question, id=question_id)
+    # questionを保持してquestion.htmlを描画
+    return render(request, 'quiz/question.html', {'question': question})
